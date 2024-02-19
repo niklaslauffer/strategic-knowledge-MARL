f29439cb1e6b02d1df5f2251b7d16f63a885408b
micah <mdc@berkeley.edu>
2024-02-19 14:07:06 -0800

    Other minor changes

--- tests/mer/test_mer_normal.py
+++ tests/mer/test_mer_normal.py
@@ -29,6 +29,52 @@ from jaxmarl.viz.overcooked_visualizer import OvercookedVisualizer
 from jaxmarl.wrappers.baselines import LogWrapperCoPolicy
 
 
+def probs2params(key, probs, config):
+
+    env = jaxmarl.make(config["ENV_NAME"], **config["ENV_KWARGS"])
+
+    action_space_size = env.action_space().n
+    network = ActorCritic(action_space_size, activation=config["ACTIVATION"])
+    key, key_a = jax.random.split(key, 2)
+
+    init_x = jnp.zeros(env.observation_space().shape)
+    init_x = init_x.flatten()
+
+    init_params = network.init(key_a, init_x)
+    # print(init_params)
+    params = jax.tree_map(lambda x :jnp.zeros(probs.shape[0:1] + x.shape), init_params)
+    params['params']['Dense_2']['bias'] = jnp.log(probs)
+
+    # dummy_obs = np.array([[1],[1],[1],[1]])
+    # pis, _ = jax.vmap(network.apply, in_axes=(0,0))(params, dummy_obs)
+
+    return params
+
+def run_fixed_coparam_setup(rng, payoffs, probs, num_trials=10):
+    config = OmegaConf.load(TESTING_DATA_DIR + 'test_mer_ff_normal.yaml')
+    config = OmegaConf.to_container(config) 
+
+    config["ENV_KWARGS"]["payoffs"] = payoffs
+        
+    rng, _rng = jax.random.split(rng, 2)
+    coparams = probs2params(_rng, probs, config)
+
+    config["COPARAMS_SOURCE"] = 'pytree'
+    config["COPARAMS_BATCH"] = coparams
+
+    return run_test_core(rng, config, num_trials)
+
+def run_test_core(rng, config, num_trials=10):
+    rngs = jax.random.split(rng, num_trials)
+    with jax.disable_jit(False):
+        train_jit = jax.jit(make_train(config))
+        out = jax.vmap(train_jit)(rngs)
+
+    train_state = out["runner_state"][0]
+
+    ### Evaluate
+    return jax.vmap(extract_normal_policy, in_axes=(None, 0))(config, train_state.params)
+
 def extract_normal_policy(config, params):
     env = jaxmarl.make(config["ENV_NAME"], **config["ENV_KWARGS"])
 
@@ -59,9 +105,9 @@ def extract_normal_policy(config, params):
 
 def test_normal_1():
 
-    num_trials = 10
+    rng = jax.random.PRNGKey(30)
 
-    config = OmegaConf.load(TESTING_DATA_DIR + '/mer_ff_normal_1.yaml')
+    config = OmegaConf.load(TESTING_DATA_DIR + '/test_mer_ff_normal.yaml')
     config = OmegaConf.to_container(config) 
 
     payoffs = jnp.array([
@@ -75,54 +121,18 @@ def test_normal_1():
     payoff_string = str(payoffs).replace('\n','')
     config['COPARAMS_FILE'] = DATA_DIR + f"{config['ENV_NAME']}_{payoff_string}_save_params50.pkl"
 
-    rng = jax.random.PRNGKey(30)
-    rngs = jax.random.split(rng, num_trials)
-    with jax.disable_jit(False):
-        train_jit = jax.jit(make_train(config))
-        out = jax.vmap(train_jit)(rngs)
-
-    train_state = out["runner_state"][0]
-
     ### Evaluate
-    pis = jax.vmap(extract_normal_policy, in_axes=(None, 0))(config, train_state.params)
-
-    for pi in pis.probs:
-        print(pi)
+    pis = run_test_core(rng, num_trials, config)
     
     # check convergence in each trial
     for prob in pis.probs:
         assert(prob[0][0] >= .99 and prob[1][1]+prob[1][2] >= .99
            or prob[1][0] >= .99 and prob[0][1]+prob[0][2] >= .99)
 
-def probs2params(key, probs, config):
-
-    env = jaxmarl.make(config["ENV_NAME"], **config["ENV_KWARGS"])
-
-    action_space_size = env.action_space().n
-    network = ActorCritic(action_space_size, activation=config["ACTIVATION"])
-    key, key_a = jax.random.split(key, 2)
-
-    init_x = jnp.zeros(env.observation_space().shape)
-    init_x = init_x.flatten()
-
-    init_params = network.init(key_a, init_x)
-    # print(init_params)
-    params = jax.tree_map(lambda x :jnp.zeros(probs.shape[0:1] + x.shape), init_params)
-    params['params']['Dense_2']['bias'] = jnp.log(probs)
-
-    # dummy_obs = np.array([[1],[1],[1],[1]])
-    # pis, _ = jax.vmap(network.apply, in_axes=(0,0))(params, dummy_obs)
-
-    return params
 
 def test_normal_2():
 
     rng = jax.random.PRNGKey(30)
-
-    num_trials = 10
-
-    config = OmegaConf.load(TESTING_DATA_DIR + 'mer_ff_normal_2.yaml')
-    config = OmegaConf.to_container(config) 
 
     payoffs = jnp.array([
         [2,-1,-1],
@@ -134,23 +144,11 @@ def test_normal_2():
                        [.01,.98,.01],
                        [.01,.01,.98]])
 
-    config["ENV_KWARGS"]["payoffs"] = payoffs
-        
-    rng, _rng = jax.random.split(rng, 2)
-    coparams = probs2params(_rng, probs, config)
+    pis = run_fixed_coparam_setup(rng, payoffs, probs)
 
-    config["COPARAMS_SOURCE"] = 'pytree'
-    config["COPARAMS_BATCH"] = coparams
-
-    rngs = jax.random.split(rng, num_trials)
-    with jax.disable_jit(False):
-        train_jit = jax.jit(make_train(config))
-        out = jax.vmap(train_jit)(rngs)
-
-    train_state = out["runner_state"][0]
-
-    ### Evaluate
-    pis = jax.vmap(extract_normal_policy, in_axes=(None, 0))(config, train_state.params)
+    # for pi in pis.probs:
+    #     print(pi)
+    
     # check convergence in each trial
     for prob in pis.probs:
         assert((prob[0][0] >= .95 and prob[1][1]+prob[1][2] >= .95)
@@ -158,4 +156,4 @@ def test_normal_2():
 
 
 if __name__ == "__main__":
-    test_normal_1()    
+    test_normal_2()
